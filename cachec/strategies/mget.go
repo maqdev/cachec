7511c6a5ed6be4b061ff6ce826b962fabe51c18d
Magomed Abdurakhmanov <maqdev@gmail.com>
2024-06-10 23:41:34 -0700

    + MGetFromCacheOrNext impl

--- cachec/strategies/mget.go
+++ cachec/strategies/mget.go
@@ -2,6 +2,11 @@ package strategies
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/maqdev/cachec/pgconvert"
+	"google.golang.org/protobuf/proto"
+	"log/slog"
 
 	"github.com/maqdev/cachec/cachec"
 )
@@ -12,163 +17,96 @@ func MGetFromCacheOrNext[PGStruct any, CacheEntityStruct any, CacheEntityIntf Ca
 	next func(indices []int) ([]*PGStruct, error),
 	convertToCache func(in *PGStruct) CacheEntityIntf) ([]*PGStruct, error) {
 
-	panic("implement me")
+	cacheResult, err := cacheClient.MultiGet(ctx, entity, keys, func() proto.Message {
+		var c CacheEntityStruct
+		return CacheEntityIntf(&c)
+	})
 
-	//cacheResult, err := cacheClient.MGet(ctx, entity, keys, func() proto.Message {
-	//	var c CacheEntityStruct
-	//	return CacheEntityIntf(&c)
-	//})
-	//
-	//// MGet failed in general
-	//if err != nil {
-	//	return nil, fmt.Errorf("%s/cacheClient.MGet failed: %w", opName, err)
-	//}
-	//
-	//result := make([]*PGStruct, len(keys))
-	//var getNext []int
-	//
-	//for index, v := range cacheResult {
-	//	if v.Err != nil {
-	//		err = pgconvert.WrapCacheError(v.Err)
-	//	}
-	//
-	//	switch {
-	//	// found in cache
-	//	case err == nil:
-	//		result[index] = v.Message.(CacheEntityIntf).ToPG()
-	//
-	//	// flagged as not found in cache, left it nil
-	//	case errors.Is(err, cachec.ErrNotFound): // do nothing
-	//
-	//	// other error
-	//	case !errors.Is(err, cachec.ErrNotCached):
-	//		return nil, fmt.Errorf("%s/cacheClient.MGet failed: %w", opName, err)
-	//
-	//	// not found in cache, load from the next DAL
-	//	default:
-	//		if getNext == nil {
-	//			getNext = make([]int, 0, len(keys))
-	//		}
-	//		getNext = append(getNext, index)
-	//
-	//		result, err := next()
-	//		if err != nil {
-	//			err = pgconvert.WrapDBError(err)
-	//
-	//			if errors.Is(err, cachec.ErrNotFound) {
-	//				if storeInCacheNotFound {
-	//					if !storeInCacheAsynchronously {
-	//						// if cacheNotFound is enabled, flag as not found in cache
-	//						err = cacheClient.FlagAsNotFound(ctx, entity, key)
-	//						if err != nil {
-	//							return nil, fmt.Errorf("%s/cacheClient.FlagAsNotFound failed: %w", opName, err)
-	//						}
-	//					} else {
-	//						go func() {
-	//							ctxAsync, cancel := context.WithTimeout(context.WithoutCancel(ctx), AsynchronousCacheTimeout)
-	//							defer cancel()
-	//
-	//							err = cacheClient.FlagAsNotFound(ctxAsync, entity, key)
-	//							if err != nil {
-	//								slog.Error("failed to flag as not found in cache %s: %s", entity.EntityName, err)
-	//							}
-	//						}()
-	//					}
-	//				}
-	//
-	//				return nil, fmt.Errorf("`%s` is not found: %w", entity.EntityName, err)
-	//			}
-	//		}
-	//
-	//		newCachedResult := convertToCache(&result)
-	//
-	//		if !storeInCacheAsynchronously {
-	//			// cache asynchronously if strategy allows
-	//			err = pgconvert.WrapCacheError(cacheClient.MultiSet(ctx, entity, key, newCachedResult))
-	//			if err != nil {
-	//				return nil, fmt.Errorf("%s/cacheClient.MultiSet failed: %w", opName, err)
-	//			}
-	//		} else {
-	//			go func() {
-	//				ctxAsync, cancel := context.WithTimeout(context.WithoutCancel(ctx), AsynchronousCacheTimeout)
-	//				defer cancel()
-	//
-	//				err = pgconvert.WrapCacheError(cacheClient.MultiSet(ctxAsync, entity, key, newCachedResult))
-	//				if err != nil {
-	//					slog.Error("failed to cache %s: %s", entity.EntityName, err)
-	//				}
-	//			}()
-	//		}
-	//
-	//		return &result, nil
-	//	}
-	//
-	//}
-	//
-	//switch {
-	//// found in cache
-	//case err == nil:
-	//	return cachedResult.ToPG(), nil
-	//
-	//// flagged as not found in cache
-	//case errors.Is(err, cachec.ErrNotFound):
-	//	return nil, fmt.Errorf("`%s` is not found: %w", entity.EntityName, err)
-	//
-	//// other error
-	//case !errors.Is(err, cachec.ErrNotCached):
-	//	return nil, fmt.Errorf("%s/cacheClient.MultiGet failed: %w", opName, err)
-	//
-	//// not found in cache, load from the next DAL
-	//default:
-	//	result, err := next()
-	//	if err != nil {
-	//		err = pgconvert.WrapDBError(err)
-	//
-	//		if errors.Is(err, cachec.ErrNotFound) {
-	//			if storeInCacheNotFound {
-	//				if !storeInCacheAsynchronously {
-	//					// if cacheNotFound is enabled, flag as not found in cache
-	//					err = cacheClient.FlagAsNotFound(ctx, entity, key)
-	//					if err != nil {
-	//						return nil, fmt.Errorf("%s/cacheClient.FlagAsNotFound failed: %w", opName, err)
-	//					}
-	//				} else {
-	//					go func() {
-	//						ctxAsync, cancel := context.WithTimeout(context.WithoutCancel(ctx), AsynchronousCacheTimeout)
-	//						defer cancel()
-	//
-	//						err = cacheClient.FlagAsNotFound(ctxAsync, entity, key)
-	//						if err != nil {
-	//							slog.Error("failed to flag as not found in cache %s: %s", entity.EntityName, err)
-	//						}
-	//					}()
-	//				}
-	//			}
-	//
-	//			return nil, fmt.Errorf("`%s` is not found: %w", entity.EntityName, err)
-	//		}
-	//	}
-	//
-	//	newCachedResult := convertToCache(&result)
-	//
-	//	if !storeInCacheAsynchronously {
-	//		// cache asynchronously if strategy allows
-	//		err = pgconvert.WrapCacheError(cacheClient.MultiSet(ctx, entity, key, newCachedResult))
-	//		if err != nil {
-	//			return nil, fmt.Errorf("%s/cacheClient.MultiSet failed: %w", opName, err)
-	//		}
-	//	} else {
-	//		go func() {
-	//			ctxAsync, cancel := context.WithTimeout(context.WithoutCancel(ctx), AsynchronousCacheTimeout)
-	//			defer cancel()
-	//
-	//			err = pgconvert.WrapCacheError(cacheClient.MultiSet(ctxAsync, entity, key, newCachedResult))
-	//			if err != nil {
-	//				slog.Error("failed to cache %s: %s", entity.EntityName, err)
-	//			}
-	//		}()
-	//	}
-	//
-	//	return &result, nil
-	//}
+	if err != nil {
+		return nil, fmt.Errorf("%s/cacheClient.MultiGet failed: %w", opName, err)
+	}
+
+	var nextIndices []int
+	result := make([]*PGStruct, len(keys))
+	for index, v := range cacheResult {
+		if v.Err != nil {
+			err = pgconvert.WrapCacheError(v.Err)
+		} else {
+			err = nil
+		}
+
+		switch {
+		// found in cache
+		case err == nil:
+			result[index] = v.Value.(CacheEntityIntf).ToPG()
+
+		// flagged as not found in cache, leave it be nil
+		case errors.Is(err, cachec.ErrNotFound): // do nothing
+
+		// other error
+		case !errors.Is(err, cachec.ErrNotCached):
+			return nil, fmt.Errorf("%s/cacheClient.MultiGet failed at %v: %w", opName, keys[index], err)
+
+		// not found in cache, load from the next DAL
+		default:
+			if nextIndices == nil {
+				nextIndices = make([]int, 0, len(keys))
+			}
+			nextIndices = append(nextIndices, index)
+		}
+	}
+
+	if len(nextIndices) > 0 {
+		var nextResult []*PGStruct
+		nextResult, err = next(nextIndices)
+
+		if err != nil {
+			err = pgconvert.WrapDBError(err)
+			return nil, fmt.Errorf("%s/next failed: %w", opName, err)
+		}
+
+		if len(nextResult) != len(nextIndices) {
+			return nil, fmt.Errorf("%s/next returned %d results, expected %d", opName, len(nextResult), len(nextIndices))
+		}
+
+		MSetCommands := make([]cachec.SetCommand, 0, len(nextIndices))
+		for i, index := range nextIndices {
+			r := nextResult[i]
+			if r != nil {
+				MSetCommands = append(MSetCommands, cachec.SetCommand{
+					Key:   keys[index],
+					Value: convertToCache(r),
+				})
+				result[index] = r
+			} else if entity.StoreNotFound {
+				MSetCommands = append(MSetCommands, cachec.SetCommand{
+					Key:         keys[index],
+					SetNotFound: true,
+				})
+			}
+		}
+
+		if len(MSetCommands) > 0 {
+			if entity.CacheAsync {
+				go func() {
+					ctxAsync, cancel := context.WithTimeout(context.WithoutCancel(ctx), AsynchronousCacheTimeout)
+					defer cancel()
+
+					_, err := cacheClient.MultiSet(ctxAsync, entity, MSetCommands)
+					if err != nil {
+						err = pgconvert.WrapCacheError(err)
+						slog.Error("cacheClient.MultiSet failed for %s: %s", entity.EntityName, err)
+					}
+				}()
+			} else {
+				_, err = cacheClient.MultiSet(ctx, entity, MSetCommands)
+				if err != nil {
+					err = pgconvert.WrapCacheError(err)
+					return nil, fmt.Errorf("%s/cacheClient.MultiSet failed: %w", opName, err)
+				}
+			}
+		}
+	}
+
+	return result, nil
 }
